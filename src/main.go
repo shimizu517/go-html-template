0ff6a49cfca3fd43a745ea90a79b74c2c68103c3
yuta <namtok.dev@gmail.com>
2022-09-18 19:59:32 +0700

    companies, layout7

--- src/main.go
+++ src/main.go
@@ -25,6 +25,8 @@ func main() {
 	generateSquareOf1to10(outDir)
 	generateListHtml(outDir)
 	generateLinksHtml(outDir)
+	generateImageHtml(outDir)
+	generateCompaniesTable(outDir)
 }
 
 func generateHtml(tmplPath string, htmlPath string, data interface{}) error {
@@ -133,7 +135,76 @@ func generateLinksHtml(outDir string) {
 	}
 }
 
-//type Layout7Item {
-//	ImagePath string
-//	Text string
-//}
+type Layout7Item struct {
+	ImagePath string
+	Text      string
+}
+
+func generateImageHtml(outDir string) {
+	filePrefix := "layout7"
+	tmplPath := filePrefix + tmplExt
+	htmlPath := outDir + filePrefix + htmlExt
+	items := []Layout7Item{
+		{"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", "Google"},
+		{"https://www.facebook.com/images/fb_icon_325x325.png", "Facebook"},
+		{"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png", "Github"},
+	}
+	tmpl := template.Must(template.ParseFiles(tmplPath))
+	f, err := os.Create(htmlPath)
+	if err != nil {
+		log.Println("generateHtml:", err)
+		return
+	}
+	err = tmpl.Execute(f, struct{ Items []Layout7Item }{Items: items})
+	if err != nil {
+		log.Println("generateHtml:", err)
+		return
+	}
+}
+
+type CompanyTableCell struct {
+	Text  string
+	Style template.CSS
+}
+
+type CompaniesTable struct {
+	TableTitle string
+	Heads      []CompanyTableCell
+	Rows       [][]CompanyTableCell
+}
+
+func generateCompaniesTable(outDir string) {
+	filePrefix := "layout_companies_table"
+	tmplPath := filePrefix + tmplExt
+	htmlPath := outDir + filePrefix + htmlExt
+	Heads := []CompanyTableCell{
+		{"NAME", ""},
+		{"SYMBOL", "text-align: center;"},
+		{"CURRENT", "text-align: center;"},
+		{"52WK HI", "text-align: center;"},
+		{"52WK LO", "text-align: center;"},
+		{"P/E RATIO", "text-align: center;"},
+	}
+	Rows := [][]CompanyTableCell{
+		{{"Intel", ""}, {"INTC", "text-align: center;"}, {"28.84", "text-align: center;"}, {"56.28", "text-align: center;"}, {"28.42", "text-align: center;"}, {"56.09", "text-align: center;"}},
+		{{"Apple", ""}, {"AAPL", "text-align: center;"}, {"152.37", "text-align: center;"}, {"182.94", "text-align: center;"}, {"129.04", "text-align: center;"}, {"24.90", "text-align: center;"}},
+		{{"Verizon", ""}, {"VZ", "text-align: center;"}, {"41.03", "text-align: center;"}, {"55.51", "text-align: center;"}, {"40.66", "text-align: center;"}, {"8.28", "text-align: center;"}},
+		{{"META", ""}, {"META", "text-align: center;"}, {"149.55", "text-align: center;"}, {"373.56", "text-align: center;"}, {"144.29", "text-align: center;"}, {"12.12", "text-align: center;"}},
+		{{"Google", ""}, {"GOOG", "text-align: center;"}, {"103.90", "text-align: center;"}, {"152.10", "text-align: center;"}, {"101.86", "text-align: center;"}, {"19.63", "text-align: center;"}},
+	}
+	tmpl := template.Must(template.ParseFiles(tmplPath))
+	f, err := os.Create(htmlPath)
+	if err != nil {
+		log.Println("generateHtml:", err)
+		return
+	}
+	err = tmpl.Execute(f, CompaniesTable{
+		TableTitle: "My tech stock picks",
+		Heads:      Heads,
+		Rows:       Rows,
+	})
+	if err != nil {
+		log.Println("generateHtml:", err)
+		return
+	}
+}
